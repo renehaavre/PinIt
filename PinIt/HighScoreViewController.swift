018f391031e7f71988ad4596cadc5933b8246502
Rene Haavre <haavre@gmail.com>
2018-11-06 00:06:15 +0200

    Set up new view + tableview in storyboard and add placeholder viewcontroller and temporary buttons to allow navigation.

---
+++ PinIt/HighScoreViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HighScoreViewController.swift
+//  PinIt
+//
+//  Created by Rene Haavre on 06/11/2018.
+//  Copyright © 2018 Rene Haavre. All rights reserved.
+//
+
+import UIKit
+
+class HighScoreViewController: UIViewController {
+
+    @IBOutlet var highScoreTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
